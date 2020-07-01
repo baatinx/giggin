1254307d876595686bccfefd750c681174d108fc
Mustafa Basit <mailtobasit74@gmail.com>
2020-07-02 00:22:39 +0530

    Adding and Removing orders from cart

--- src/giggin/components/orders.cljs
+++ src/giggin/components/orders.cljs
@@ -1,5 +1,21 @@
-(ns giggin.components.orders)
+(ns giggin.components.orders
+  (:require [giggin.state :as state]))
 
 (defn orders
   []
-  [:div "orders"])
+  [:aside
+   [:div.order
+    [:div.body
+     (for [[id quantity] @state/orders]
+       [:div.item {:key id}
+        [:div.img 
+         [:img {:src (get-in @state/gigs [id :img])
+                :alt (get-in @state/gigs [id :title])}]]
+        [:div.content  
+         [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quantity)]]
+        [:div.action
+         [:div.price (* (get-in @state/gigs [id :price]) quantity)]
+         [:button.btn.btn--link.tooltip 
+          {:data-tooltip "Remove"
+           :on-click (fn [] (swap! state/orders dissoc id))}
+          [:i.icon.icon--cross]]]])]]])
